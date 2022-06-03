005652d1cdf7c891125592c87c37ce2240ddeea9
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-06-03 12:29:07 +0800

    添加了标签栏

--- app/src/main/java/com/example/class2021_2022_2_androidappdeveloptraining/OrderAndShoppingActivity.java
+++ app/src/main/java/com/example/class2021_2022_2_androidappdeveloptraining/OrderAndShoppingActivity.java
@@ -2,6 +2,7 @@ package com.example.class2021_2022_2_androidappdeveloptraining;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -10,6 +11,8 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.example.class2021_2022_2_androidappdeveloptraining.adapter.ViewAdapter;
 import com.example.class2021_2022_2_androidappdeveloptraining.fragment.CaipinFramgment;
 import com.example.class2021_2022_2_androidappdeveloptraining.fragment.OrderFragment;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 
@@ -17,12 +20,21 @@ import java.util.ArrayList;
 public class OrderAndShoppingActivity extends FragmentActivity {
 
     private ViewPager2 vp_content;
+    private com.google.android.material.tabs.TabLayout tabLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_and_shopping_activity);
         initPager();
+
+        tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, vp_content, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(position == 0 ? "点餐" : "购物车");
+            }
+        }).attach();
     }
 
     private void initPager() {
