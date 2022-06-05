befe3042f2824e1a62ed416545effbd958e724a5
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-06-05 19:32:43 +0800

    rename

--- app/src/main/java/com/example/class2021_2022_2_androidappdeveloptraining/fragment/ShoppingCardFragment.java
+++ app/src/main/java/com/example/class2021_2022_2_androidappdeveloptraining/fragment/ShoppingCardFragment.java
@@ -44,8 +44,9 @@ public class ShoppingCardFragment extends Fragment {
                 shoppingCard.addItem(new Dish((int) (Math.random() * 100), Integer.toString((int) (Math.random() * 100)), (int) (Math.random() * 100), (float) (Math.random() * 100)), 1);
                 for (ShoppingCardItem item :
                         shoppingCard.getShoppingCardItems()) {
-                    System.out.println(item.getDish().getId());
+                    System.out.println(item.getDish().getId() + "" + item.getDish().getName());
                 }
+                System.out.println();
                 listViewAdapter.notifyDataSetChanged();
             }
         });
